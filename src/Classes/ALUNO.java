0c6ebef1316366177a8eb9e82d6a9f823f2cc84d
LucasPedrosoTI <Lucas.PedrosoTI@gmail.com>
2020-10-07 20:06:09 -0300

    teste

--- src/Classes/ALUNO.java
+++ src/Classes/ALUNO.java
@@ -2,7 +2,7 @@ package Classes;
 
 public class ALUNO {
     String nome, endereco, data_nasc, telefone,id_matricula,data_matricula;
-    float peso, altura;
+    float peso, altura, teste;
     
     
 }

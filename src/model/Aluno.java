4590c89c7b3acbe8ec01a9f268d29b1c48c83cc4
LucasPedrosoTI <Lucas.PedrosoTI@gmail.com>
2020-10-25 12:47:16 -0300

    Atualizado os nomes das pastas

---
+++ src/model/Aluno.java
@@ -0,0 +1,13 @@
+package model;
+
+import java.util.List;
+
+public class Aluno {
+    private List<Matricula> matriculas;
+    private String nome, endereco, dataNascimento;
+    private Telefone telefone;
+    private double peso, altura, imc;
+
+    
+            
+}
---
+++ src/model/Professor.java
@@ -0,0 +1,11 @@
+package model;
+
+import java.util.List;
+
+public class Professor {
+    private int professorId;
+    private String nome, rg,data_nascimento, titulacao;
+    private List<Telefone> telefones;
+    private List<Turma> turmas;
+    
+}

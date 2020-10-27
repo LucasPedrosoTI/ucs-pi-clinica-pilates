2b5a980869454745eac2fb7a38090c36bdbd989d
LucasPedrosoTI <Lucas.PedrosoTI@gmail.com>
2020-10-26 22:06:17 -0300

    configuração inicial de login e função para sair e fechar o sistema

--- src/model/Usuario.java
+++ src/model/Usuario.java
@@ -22,6 +22,31 @@ public class Usuario {
         this.tipoAcesso = tipoAcesso;
     }
 
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public Acesso getTipoAcesso() {
+        return tipoAcesso;
+    }
+
+    public void setTipoAcesso(Acesso tipoAcesso) {
+        this.tipoAcesso = tipoAcesso;
+    }
+
+    
     
     
 }
